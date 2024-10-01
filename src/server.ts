71f6ff93257389a3268530357556513a236a3441
Mateus Alves <contatomateusalves@hotmail.com>
2024-10-01 19:42:19 -0300

    🚀 initial commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(urlencoded({ extended: true }));
+server.use(express.json());
+
+// routes
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Servidor rodando em ${process.env.BASE_URL}`);
+});
