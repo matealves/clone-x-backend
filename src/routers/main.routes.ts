e7ee13e50975d7c8456429c821109c64830a91d8
Mateus Alves <contatomateusalves@hotmail.com>
2024-10-02 13:16:25 -0300

    feat: planning routes

--- src/routers/main.routes.ts
+++ src/routers/main.routes.ts
@@ -5,3 +5,28 @@ export const mainRouter = Router();
 
 // test
 mainRouter.get("/ping", pingController.ping);
+// mainRouter.get("/privateping", pingController.ping);
+
+// auth
+// mainRouter.post("/auth/signup");
+// mainRouter.post("/auth/signin");
+
+// tweet
+// mainRouter.post("/tweet");
+// mainRouter.get("/tweet/:id");
+// mainRouter.get("/tweet/:id/answers");
+// mainRouter.post("/tweet/:id/like");
+
+// user
+// mainRouter.get("/user/:username");
+// mainRouter.get("/user/:username/tweets");
+// mainRouter.post("/user/:username/follow");
+// mainRouter.put("/user");
+// mainRouter.put("/user/avatar");
+// mainRouter.put("/user/cover");
+
+// general
+// mainRouter.get("/feed");
+// mainRouter.get("/search");
+// mainRouter.get("/trending");
+// mainRouter.get("/suggestions");
